a92f968a1f1d98557f4f273841764b9b2dedfe45
alpjakop <alp.jakop@insa-rennes.fr>
2024-12-04 11:29:42 +0100

    Script updated

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -3,48 +3,17 @@ IMAGE_NAME="jupyter-notebook-image"
 
 docker build -t $IMAGE_NAME .
 
-echo "Executing Data-Extraction.ipynb..."
-docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME \
-    jupyter nbconvert --execute --to notebook --inplace Data-Extraction.ipynb
-
-if [ $? -ne 0 ]; then
-    echo "Data-Extraction notebook execution failed, exiting."
-    exit 1
-fi
-
-echo "Executing Reproduce.ipynb..."
-docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME \
-    jupyter nbconvert --execute --to notebook --inplace Reproduce.ipynb
-
-if [ $? -ne 0 ]; then
-    echo "Reproduce notebook execution failed, exiting."
-    exit 1
-fi
-
-echo "Converting Reproduce.ipynb to HTML..."
-docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME \
-    jupyter nbconvert --to html Reproduce.ipynb
-
-if [ $? -ne 0 ]; then
-    echo "HTML conversion failed, exiting."
-    exit 1
-fi
-
-echo "Executing Replicate.ipynb..."
-docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME \
-    jupyter nbconvert --execute --to notebook --inplace Replicate.ipynb
-
-if [ $? -ne 0 ]; then
-    echo "Replicate notebook execution failed, exiting."
-    exit 1
-fi
-
-echo "Converting Replicate.ipynb to HTML..."
-docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME \
+echo "Executing all notebooks and generating HTML outputs..."
+docker run --rm -v ./notebooks:/home/jovyan/work $IMAGE_NAME bash -c "
+    jupyter nbconvert --execute --to notebook --inplace Data-Extraction.ipynb && \
+    jupyter nbconvert --execute --to notebook --inplace Reproduce.ipynb && \
+    jupyter nbconvert --to html Reproduce.ipynb && \
+    jupyter nbconvert --execute --to notebook --inplace Replicate.ipynb && \
     jupyter nbconvert --to html Replicate.ipynb
+"
 
 if [ $? -ne 0 ]; then
-    echo "HTML conversion failed, exiting."
+    echo "Workflow execution failed, exiting."
     exit 1
 fi
 
